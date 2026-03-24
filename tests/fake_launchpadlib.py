3186da2893ce00cb7b241ee81f7ea64bed912849
Bjorn Tillenius <bjorn@canonical.com>
2026-03-24 15:37:54 +0100

    chore: add AGPL v3 license information

--- tests/fake_launchpadlib.py
+++ tests/fake_launchpadlib.py
@@ -1,3 +1,6 @@
+# Copyright 2026 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
 """Fake that mimics the launchpadlib object model.
 
 ``LaunchpadClient`` talks to launchpadlib objects (``Launchpad``,
